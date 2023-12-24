1f8a0f17e716c2b36530e9e518891a1eef82ec54
morronepablo <morronepablo@gmail.com>
2023-12-24 03:44:32 -0300

    product and component complete

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,5 +1,11 @@
 import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
 
 export default function NewProduct() {
-  return <Layout>new Product</Layout>;
+  return (
+    <Layout>
+      <h1>New Product</h1>
+      <ProductForm />
+    </Layout>
+  );
 }
