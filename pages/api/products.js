05b154cf0157e8bad8f2fef48292dfb24b35b977
morronepablo <morronepablo@gmail.com>
2023-12-27 02:25:26 -0300

    medificando nomnre de archivo

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,4 +1,4 @@
-import { Product } from "@/models/Products";
+import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handle(req, res) {
